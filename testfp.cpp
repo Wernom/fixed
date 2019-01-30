26167823b0850f4c224e0e8ebddb2d03ee0c5200
Artthegor <jeremy.thiebaud@outlook.fr>
2019-01-30 16:01:15 +0100

    Add tests float() & double()
    add tests float() and double() in testfp.cpp

--- testfp.cpp
+++ testfp.cpp
@@ -20,6 +20,26 @@ TEST(fixedTest, constructorDouble){
     EXPECT_EQ(f.value, 3180);
 }
 
+
+//Test fixed<Int, Frac>::operator float() const
+TEST(fixedTest, floatValue){
+    double x = 12.42;
+    fp::fixed<8,8> f(x);
+    EXPECT_EQ(f.value, 3180);
+    EXPECT_NEAR((float)(12.421875),float(f),0.2);
+
+}
+
+//Test fixed<Int, Frac>::operator double() const
+TEST(fixedTest, doubleValue){
+    double x = 12.42;
+    fp::fixed<8,8> f(x);
+    EXPECT_EQ(f.value, 3180);
+    EXPECT_NEAR((double)(12.421875),float(f),0.2);
+
+}
+
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
